5dacf5fe1a2732f0505f56689c8c828d3534d117
Julieta Ramirez Solis Nguyen <julieta.rammirez27@gmail.com>
2023-04-24 19:52:05 -0700

    Changes to be committed:

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,33 @@ function fetchWeatherData(city) {
         })
         .catch(error => console.error("Error fetching weather data: ", error));
 }
+
+function displayWeatherData(data) {
+    cityNameDate.textContent = `${data.city.name} and ${dayjs().format("MM/DD/YYYY")}`;
+
+    const currentWeatherData = data.list[0];
+    currentWeatherCard.querySelector(".temp").textContent = `Temp: ${currentWeatherData.main.temp} °F`;
+    currentWeatherCard.querySelector(".wind").textContent = `Wind: ${currentWeatherData.wind.speed} mph`;
+    currentWeatherCard.querySelector(".humidity").textContent = `Humidity: ${currentWeatherData.main.humidity}%`;
+
+    for (let i = 0; i < 5; i++) {
+        const forecastData = data.list[(i + 1) * 8];
+        const card = document.createElement("div");
+        card.classList.add("col");
+
+        card.innerHTML = `
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Date: ${dayjs(forecastData.dt_txt).format("MM/DD/YYYY")}</h5>
+                    <p class="temp">Temp: ${forecastData.main.temp} °F</p>
+                    <p class="wind">Wind: ${forecastData.wind.speed} mph</p>
+                    <p class="humidity">Humidity: ${forecastData.main.humidity}%</p>
+                   
+                </div>
+            </div>
+            </div>
+        `;
+        fiveDayForecast.appendChild(card);
+    }
+}
